2014cc1bd00f95c4a6d0dce32b2d961dfde5b26a
Rossen Georgiev <rossen@rgp.io>
2019-06-16 16:37:05 +0100

    no py2 support

--- setup.py
+++ setup.py
@@ -34,8 +34,6 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
