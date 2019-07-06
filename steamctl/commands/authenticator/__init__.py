0112fd696173d21e6740c85b588599e0e2f2b225
Rossen Georgiev <rossen@rgp.io>
2019-07-06 12:56:31 +0100

    fix #1; add autocomplete to 'authenticator code' cmd

--- steamctl/commands/authenticator/__init__.py
+++ steamctl/commands/authenticator/__init__.py
@@ -1,9 +1,7 @@
 
 import sys
 from steamctl.argparser import register_command
-from steamctl.utils.storage import UserDataFile, UserCacheFile
-from argcomplete import warn
-
+from steamctl.utils.storage import UserDataDirectory
 
 epilog = """\
 """
@@ -38,7 +36,11 @@ def cmd_parser(cp):
     scp.add_argument('--utc', action='store_true', help='Show datetime in UTC')
     scp.set_defaults(_cmd_func=__name__ + '.cmds:cmd_authenticator_list')
 
+    def account_autocomplete(prefix, parsed_args, **kwargs):
+        return [userfile.filename[:-5]
+                for userfile in UserDataDirectory('authenticator').list_files('*.json')]
+
     scp = sub_cp.add_parser("code", help="Generate auth code")
-    scp.add_argument('account', nargs='?', type=str, help='Account name')
+    scp.add_argument('account', type=str, help='Account name').completer = account_autocomplete
     scp.set_defaults(_cmd_func=__name__ + '.cmds:cmd_authenticator_code')
 
