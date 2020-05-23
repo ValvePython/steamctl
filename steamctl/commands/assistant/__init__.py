bfb6b8d014d77634d4fc577d66714407fbee1d6f
Rossen Georgiev <rossen@rgp.io>
2020-05-23 12:26:43 +0100

    assistant: add spring cleaning 2020 helper

--- steamctl/commands/assistant/__init__.py
+++ steamctl/commands/assistant/__init__.py
@@ -20,3 +20,6 @@ def cmd_parser(cp):
 
     scp_i = sub_cp.add_parser("idle-cards", help="Automatic idling for game cards")
     scp_i.set_defaults(_cmd_func=__name__ + '.card_idler:cmd_assistant_idle_cards')
+
+    scp_i = sub_cp.add_parser("springcleaning2020", help="Complete 'Dust off your library with DEWEY!'")
+    scp_i.set_defaults(_cmd_func=__name__ + '.springcleaning2020:cmd_assistant_sc2020')
