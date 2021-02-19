b19396a193ec00db79bf470bae55361b33db388f
Rossen Georgiev <rossen@rgp.io>
2021-02-19 23:49:53 +0000

    refactor steam cloud list

--- steamctl/commands/cloud/gcmds.py
+++ steamctl/commands/cloud/gcmds.py
@@ -19,6 +19,7 @@ webapi._make_requests_session = make_requests_session
 
 LOG = logging.getLogger(__name__)
 
+
 @contextmanager
 def init_client(args):
     s = CachingSteamClient()
@@ -26,33 +27,44 @@ def init_client(args):
     yield s
     s.disconnect()
 
+
+def _get_cloud_files(s, app_id):
+    job_id = s.send_um('Cloud.EnumerateUserFiles#1',
+                       {'appid': app_id,
+                        'extended_details': True,
+                        })
+
+    files = []
+    total_files, n_files, total_size = None, 0, 0
+
+    while total_files != n_files:
+        msg = s.wait_msg(job_id, timeout=10)
+
+        if not msg:
+            raise SteamError("Failed listing UFS files", EResult.Timeout)
+        if msg.header.eresult != EResult.OK:
+            raise SteamError("Failed listing UFS files", EResult(msg.header.eresult))
+
+        total_files = msg.body.total_files
+        n_files += len(msg.body.files)
+
+        for entry in msg.body.files:
+            files.append(entry)
+            total_size += entry.file_size
+
+    return files, total_files, total_size
+
 def cmd_cloud_list(args):
     with init_client(args) as s:
-        job_id = s.send_um('Cloud.EnumerateUserFiles#1',
-                           {'appid': args.app_id,
-                            'extended_details': True,
-                            })
+        files, n_files, total_size = _get_cloud_files(s, args.app_id)
 
-        total_files, n_files = None, 0
-
-        while total_files != n_files:
-            msg = s.wait_msg(job_id, timeout=10)
-
-            if not msg:
-                raise SteamError("Failed listing UFS files", EResult.Timeout)
-            if msg.header.eresult != EResult.OK:
-                raise SteamError("Failed listing UFS files", EResult(msg.header.eresult))
-
-            total_files = msg.body.total_files
-            n_files += len(msg.body.files)
-
-            for entry in msg.body.files:
-                if not args.long:
-                    print(entry.filename)
-                else:
-                    print("{} - size:{:,d} sha1:{}".format(
-                            entry.filename,
-                            entry.file_size,
-                            entry.file_sha,
-                            )
-                          )
+        for entry in files:
+            if not args.long:
+                print(entry.filename)
+            else:
+                print("{} - size:{:,d} sha1:{}".format(
+                        entry.filename,
+                        entry.file_size,
+                        entry.file_sha,
+                        )
+                      )
